6fc89cba1c3cc41477c3cec58fe318b4071783c6
rahil471 <rahilsk47@gmail.com>
2015-05-31 21:16:33 +0530

    initial commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('angularTable', ['angularUtils.directives.dirPagination']);
+
+app.controller('listdata',function($scope, $http){
+	$scope.users = []; //declare an empty array
+	$http.get("mockJson/mock.json").success(function(response){ 
+		$scope.users = response;  //ajax request to fetch data into $scope.data
+	});
+	
+	$scope.sort = function(keyname){
+		$scope.sortKey = keyname;   //set the sortKey to the param passed
+		$scope.reverse = !$scope.reverse; //if true make it false and vice versa
+	}
+});
